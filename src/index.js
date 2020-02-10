2d08e091aa5e0081fb08d4186b2e16dcbefa15dc
Alexandre Ruban <alexandre.ruban@gmail.com>
2020-02-10 12:17:36 +0100

    tasks are now draggable thanks to DragDropContext, Droppable and Draggable

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DragDropContext } from "react-beautiful-dnd"
 import initialData from './initial-data'
 import Column from './column'
 
 class App extends React.Component {
   state = initialData;
 
-  render() {
-    return this.state.columnOrder.map((columnId) => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+  onDragEnd = () => {
+    // Todo : reorder our columns
+  }
 
-      return <Column key={column.id} column={column} tasks={tasks} />
-    })
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map((columnId) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+          return <Column key={column.id} column={column} tasks={tasks} />
+        })}
+    </DragDropContext>
+    )
   }
 }
 
--- src/task.js
+++ src/task.js
@@ -1,16 +1,31 @@
 import React from "react"
 import styled from "styled-components"
+import { Draggable } from "react-beautiful-dnd"
 
 const Container = styled.div`
   border: solid 1px lightgrey;
   padding: 8px;
   margin-bottom: 8px;
   border-radius: 2px;
+  background-color: white;
 `
 
 class Task extends React.Component {
   render() {
-    return <Container>{this.props.task.content}</Container>
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            innerRef={provided.innerRef}
+            ref={provided.innerRef}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    )
   }
 }
 
